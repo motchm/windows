22191c3d0fdeb0f736a06225b773c1c34df9049b
motchm <92804075+motchm@users.noreply.github.com>
2022-08-05 14:43:24 +0200

    Add files via upload

---
+++ AddDomainUserCSV.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module ActiveDirectory
+
+$securepassword = ConvertTo-SecureString "CHANGEME" -AsPlainText -Force
+
+$filepath = Read-Host -Prompt "Please enter the path to your CSV file"
+
+$users = Import-Csv $filepath
+
+ForEach ($user in $users) {
+
+    $fname = $user.'First Name'
+    $lname = $user.'Last Name'
+    $jtitle = $user.'Job Title'
+    $ophone = $user.'Office Phone'
+    $email = $user.'Email Address'
+    $desc = $user.Description
+    $OUpath = $user.'Organizational Unit'
+
+    New-ADUser -Name "$fname §lname" -GivenName $fname -Surname $lname -UserPrincipalName "$fname.$lname" -Path $OUpath -AccountPassword $securepassword -ChangePasswordAtLogon $true -OfficePhone $ophone -Description $desc -Enabled $true -EmailAddress $email
+
+    echo "Accout has been created for $fname $lname in $OUpath"
+}
